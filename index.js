405f313d0663282cc94a260ac10f3d8307fd9f5c
americanman <snowboardracer7@gmail.com>
2019-03-19 16:05:11 -0400

    Init commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// class constructor
+class Car {
+  drive() {
+    console.log("room-doom-doom")
+  }
+}
+
+// old school ES5 constructor
+function Moto() {
+}
+
+
+// add methods with prototype to both constructors
+Moto.prototype.drive = function() {return console.log("drr-drr-drr")}
+Car.prototype.stop = function() {return console.log("shooooo!")}
+const car1 = new Car
+const moto1 = new Moto
+
+console.log(car1.drive())
+console.log(car1.stop())
+console.log(moto1.drive())
